f49f90d979e06fc7bd6c1656a6f47fae53d38aaa
khalil.ay <ka2916544@gmail.com>
2025-10-25 17:03:54 +0200

    fix: correct parameter access in getOrder and update response format

--- src/approach-1/controllers/orders.controller.js
+++ src/approach-1/controllers/orders.controller.js
@@ -1,10 +1,12 @@
 import Order from '../../shared/modules/orders.js';
 
 export const getOrder = async (req,res) =>{
-    const orderId = req.parms.id
+    const orderId = req.params.id;
+    if (!orderId) return res.status(404).json({err:"order not found"})
+
     const order = await Order.findByPk(orderId);
 
-    res.json({order})
+    res.json(order.toJSON())
 }
 
 export const createOrder = async (req,res) =>{
