dd3d3ef8e6efdebaed2cfb10949eba5adc61e3f9
bayan2002 <bayanabdelbaryq@gmail.com>
2025-11-01 11:27:41 +0200

    feat: implement Approach 2 with Node.js clustering, order worker, payment worker and Redis stock cache

--- src/approach-2/index.api.js
+++ src/approach-2/index.api.js
@@ -12,42 +12,52 @@ import {
 } from '../shared/config/db.js';
 import { redis } from '../shared/config/redis.js';
 import { apiRateLimiter } from '../shared/middleware/rateLimiter.js';
+// import { setupCluster } from '../shared/config/cluster.js';
 import authRoutes from '../shared/routes/auth.route.js';
 import orderRoutes from '../shared/routes/orders.route.js';
 import productsRoute from '../shared/routes/products.route.js';
 import testRouter from '../shared/routes/test.js';
-const app = express();
-const port = 3000;
 
-
-app.use(express.json())
-app.use(express.static('public'));
-app.use(apiRateLimiter);
-app.set("trust proxy", 1);
-
-
-app.use((req, res, next) => {
-  const instanceId = process.env.INSTANCE_ID || `PID-${process.pid}`;
-  res.setHeader('X-Instance-ID', instanceId);
-  console.log(`🧩 ${instanceId} -> ${req.method} ${req.originalUrl}`);
-  next();
-});
-
-await connectDB();
-await sequelize.sync({ alter: true });
-
-
-// routes
-app.use('/',testRouter )
-app.use('/auth',authRoutes)
-app.use('/order',orderRoutes)
-app.use('/products',productsRoute)
-app.get("/health/redis", async (_req, res) => {
-    const pong = await redis.ping();
-    res.json({ ok: pong === "PONG" });
-});
-
-
-app.listen(port, () => {
-    // console.log(`Server running on http://localhost:${port}`);
-});
+// const workerCount = process.env.HTTP_WORKERS || 4;
+// for clustering
+// const startServer = async () => {
+  const app = express();
+  const port = 3000;
+  
+  
+  app.use(express.json())
+  app.use(express.static('public'));
+  app.use(apiRateLimiter);
+  app.set("trust proxy", 1);
+  
+  
+  app.use((req, res, next) => {
+    const instanceId = process.env.INSTANCE_ID || `PID-${process.pid}`;
+    res.setHeader('X-Instance-ID', instanceId);
+    console.log(`🧩 ${instanceId} -> ${req.method} ${req.originalUrl}`);
+    next();
+  });
+  
+  await connectDB();
+  await sequelize.sync({ alter: true });
+  // for clustering
+  // await sequelize.sync({ force: false });
+  
+  // routes
+  app.use('/',testRouter )
+  app.use('/auth',authRoutes)
+  app.use('/order',orderRoutes)
+  app.use('/products',productsRoute)
+  app.get("/health/redis", async (_req, res) => {
+      const pong = await redis.ping();
+      res.json({ ok: pong === "PONG" });
+  });
+  
+  app.listen(port, () => {
+      // console.log(`Server running on http://localhost:${port}`);
+      // for clustering
+      // console.log(`🚀 HTTP Worker ${process.pid} listening on http://localhost:${port}`);
+  });
+// }
+// for clustering
+// setupCluster(workerCount, startServer, 'HTTP');
