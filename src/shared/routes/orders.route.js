68182a1986ee25267221125ad90d2195dcd2a764
khalil.ay <ka2916544@gmail.com>
2026-07-14 18:05:27 +0300

    chore: update src/shared/routes/orders.route.js

--- src/shared/routes/orders.route.js
+++ src/shared/routes/orders.route.js
@@ -7,7 +7,7 @@ import { verifyToken } from '../middleware/auth.js';
 import { queueLimiterMiddleware } from '../middleware/processHandlerLimit.js';
 const router = express.Router()
 router.get('/:id',getOrder)
-router.post('/buy',verifyToken,queueLimiterMiddleware,idempotency,buy)
+router.post('/buy',verifyToken,idempotency,buy)
 router.post('/buy-flash',verifyToken,queueLimiterMiddleware,idempotency,flashBuy)
 router.get('/:orderId/status',getOrderStatus)
 export default router;
